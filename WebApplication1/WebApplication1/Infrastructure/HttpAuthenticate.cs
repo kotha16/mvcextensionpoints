d14b09bf7f55acd1096946daac262bddae9ce162
Kotha, Shashanka <shashanka.kotha@fmr.com>
2016-02-23 15:48:36 -0500

    More changes

---
+++ WebApplication1/WebApplication1/Infrastructure/HttpAuthenticate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace WebApplication1.Infrastructure
+{
+    public class HttpAuthenticate : FilterAttribute, IAuthenticationFilter
+    {
+        private string _username { get; set; }
+        private string _password { get; set; }
+        public HttpAuthenticate(string username, string password)
+        {
+            _username = username;
+            _password = password;
+        }
+
+        
+        //public Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
+        //{
+        //    // filterContext.Result = new HttpUnauthorizedResult();
+
+        //}
+
+        //public Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+        //{
+        //    // use this to customize the result that's sent back.
+        //}
+
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Address.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using WebApplication1.Infrastructure;
+
+namespace WebApplication1.Models
+{
+    public class Address : IValidatableObject
+    {
+        /*
+        Attributes
+        Reusable, SOC, Inheritance
+
+        Downside: Cross Property Validation
+
+        IValidatableObject
+        Easy Cross Property Validation
+        Places Logic in Model...Limited Reusability
+
+        MVC: Validation during Model Binder.... Check Validation Attributes, Check IValidatable Object
+
+        Populate ModelState
+        */
+
+        [BirthdateValidator]
+        public DateTime? Dob { get; set; }
+        public DateTime? Doe { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+            if (Dob.Value > Doe.Value)
+            {
+                results.Add(new ValidationResult("Dob cannot be greater then Doe"));
+
+            }
+
+            return results;
+        }
+    }
+}
